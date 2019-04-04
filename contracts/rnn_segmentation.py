a39c67234b2eca6538592857bd72cd10d19e97ff
DanielLongo <danieldominiclongo@gmail.com>
2019-04-04 10:17:18 -0700

    updates

---
+++ contracts/rnn_segmentation.py
@@ -0,0 +1,8 @@
+import torch
+from torch import nn
+import numpy as np
+
+class RnnSegmentation(nn.Module):
+    def __init__(self):
+        super(RnnSegmentation, self).__init__()
+    
--- load_info.py
+++ load_info.py
@@ -1,14 +1,37 @@
 import pandas as pd
+import numpy as np
+from load_contracts import preprocess_text, read_contract
 import xlrd
 
 def load_info_xlsx(filename):
     df = pd.read_excel(filename)
     out = pd.concat([df["File Name"], df["Security Name"], df["Number"]], axis=1)
-    print(out["File Name"])
+    out = out[np.isfinite(df["Number"])]
+    out = out.dropna(how='any')
+    out["Security Name"] = out["Security Name"].apply(preprocess_text)
+    out["Number"] = out["Number"].apply("{:.20g}".format).apply(str).apply(preprocess_text)
+    return out.values
+    # print(out["File Name"])
     # print("out", out)
     # print(df["Security Name"].unique())
 
+def generate_map(y, num_examples=10, filepath="./contracts/"):
+    assert(y.shape[0] >= num_examples), "Not enough examples"
+    for entry in y:
+        cur_filename = entry[0]
+        cur_name = entry[1]
+        cur_number = entry[2]
+        text = read_contract(filepath + cur_filename)
+        matches_name = [(i, i+len(cur_name)) for i in range(len(text)) if text[i:i+len(cur_name)] == cur_name]
+        for match in matches_name:
+            print(match)
+            print(text[match[0]:match[1]])
+        print(entry)
+
+
 
 if __name__ == "__main__":
-    load_info_xlsx("rs1 database AN.xlsx")
+    y = load_info_xlsx("rs1 database AN.xlsx")
+    print("x shape", y.shape)
+    generate_map(y)
     print("Finished")
